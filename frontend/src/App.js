2a8b53d187bbb80e56213feed035f9a02e6223d7
sharonzolty <sharonzolty@gmail.com>
2021-04-16 18:30:27 +0300

    Update Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 // import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Contact from './Pages/Contact';
-import Navbar from './Components';
+import Navbar from './Navbar';
 import AllClubs from './Pages/AllClubs';
 import Home from './Pages';
 import signin from './Pages/signIn';
