29942ba61eb5aa17e0c5222887d3b94983714801
sharonzolty <69958637+sharonzolty@users.noreply.github.com>
2021-04-17 14:35:56 +0300

    Sharon (#17)
    * Add Navbar
    
    * Update Navbar
    
    * Update Navbar
    
    * fix Navbar
    
    * fix Navbar

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { NavLink as Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+function Navbar() {
+  return (
+    <>
+      <Nav>
+        <NavLink to="/" exact activeStyle>
+          Home
+        </NavLink>
+        <NavLink to="/allClubs" activeStyle>
+          All Clubs
+        </NavLink>
+        <NavLink to="/contact" activeStyle>
+          Contact Us
+        </NavLink>
+        <NavSignIn to="/signin" activeStyle>
+          Sign In
+        </NavSignIn>
+      </Nav>
+    </>
+  );
+}
+
+export default Navbar;
+export const Nav = styled.nav`
+  background: #000000;
+  height: 60px;
+`;
+
+export const NavLink = styled(Link)`
+  float: left;
+  color: #ffffff;
+  padding: 20px 10px 10px 25px;
+  margin: 0px 0px 0px 25px;
+  display: inline-grid;
+  align-items: center;
+  text-decoration: none;
+  font: Roboto;
+  font-weight: bold;
+  font-size: 14px;
+  white-space: nowrap;
+  &.active {
+    color: #2561da;
+  }
+`;
+
+export const NavSignIn = styled(Link)`
+  color: #ffffff;
+  float: right;
+  padding: 20px 50px 25px 25px;
+  margin: 0px 0px 0px 25px;
+  align-items: center;
+  text-decoration: none;
+  font: Roboto;
+  font-weight: bold;
+  font-size: 14px;
+  &.active {
+    color: #2561da;
+  }
+`;
---
+++ frontend/src/Pages/signIn.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+function signin() {
+  return (
+    <>
+      <Container>
+        <ComponentContainer>
+          <div>Name</div>
+        </ComponentContainer>
+      </Container>
+    </>
+  );
+}
+
+export default signin;
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  width: 100%;
+  grid-gap: 10px;
+`;
+const ComponentContainer = styled.div`
+  /* padding: 0 10px; */
+`;
