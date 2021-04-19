a3a64a5ee4a6e111322d926212f35b09da65ead8
barshopen <bar.shopen@gmail.com>
2021-04-19 23:09:03 +0300

    .

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -2,57 +2,47 @@ import React, { useState } from 'react';
 import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const SearchBox = styled.input`
-  margin: 7px 15px;
-  padding: 8px 8px;
-  font-size: 14px;
-  width: 15%;
-  max-width: 20%;
-  border-style: solid;
-  border-width: 2px;
-  border-color: #000;
-  border-radius: 20px;
-`;
-
-const SearchButton = styled.button`
-  color: #fff;
-  margin-right: 200px;
-`;
-
 export const Nav = styled.nav`
+  display:grid;
+  grid-template-columns:repeat(10, 1fr);
+  grid-template-areas:
+      "nl1 nl2 nl3  sb sb sb sb sb  nl4 nl5 ";
+  grid-gap:40px;
+  align-items:center;
   background: #000000;
   height: 60px;
+  font-size: 14px;
+  font: Roboto;
 `;
 
 export const NavLink = styled(Link)`
   float: left;
   color: #ffffff;
-  padding: 20px 10px 10px 25px;
-  margin: 0px 0px 0px 25px;
   display: inline-grid;
   align-items: center;
   text-decoration: none;
-  font: Roboto;
   font-weight: bold;
-  font-size: 14px;
   white-space: nowrap;
+  grid-area: ${(props) => props.gridArea};
   &.active {
     color: #2561da;
   }
+
+`;
+const NavBarSearch = styled.form`
+  grid-area: sb;
+
+`;
+const SearchBox = styled.input`
+  margin: 11px 15px;
+  padding: 8px 8px;
+  width:300px;
+  border-style: solid; border-width: 2px; border-color: #000; border-radius: 20px;
 `;
 
-export const NavSignIn = styled(Link)`
-  color: #ffffff;
-  float: right;
-
-  margin: -30px 50px 0px 0px;
-  text-decoration: none;
-  font: Roboto;
-  font-weight: bold;
-  font-size: 14px;
-  &.active {
-    color: #2561da;
-  }
+const SearchButton = styled.button`
+  color: #fff;
+  margin-right: 200px;
 `;
 
 function Navbar() {
@@ -65,19 +55,20 @@ function Navbar() {
   return (
     <>
       <Nav>
-        <NavLink to="/" exact>
+
+        <NavLink to="/" exact gridArea="nl1">
           Home
         </NavLink>
-        <NavLink to="/allClubs">
+        <NavLink to="/allClubs" gridArea="nl2">
           All Clubs
         </NavLink>
-        <NavLink to="/contact">
+        <NavLink to="/contact" gridArea="nl3">
           Contact Us
         </NavLink>
-        <NavLink to="/createnewclub" activeStyle>
+        <NavLink to="/createnewclub" gridArea="nl4">
           Create Club
         </NavLink>
-        <form>
+        <NavBarSearch>
           <SearchBox
             placeholder="Search..."
             type="search"
@@ -85,13 +76,12 @@ function Navbar() {
             onChange={(e) => setSearchData(e.target.value)}
           />
           <SearchButton onClick={(e) => handleClick(e)} type="button">
-            {' '}
             Search
           </SearchButton>
-        </form>
-        <NavSignIn to="/signin" activeStyle>
+        </NavBarSearch>
+        <NavLink to="/signin" activeStyle gridArea="nl5">
           Sign In
-        </NavSignIn>
+        </NavLink>
       </Nav>
     </>
   );
