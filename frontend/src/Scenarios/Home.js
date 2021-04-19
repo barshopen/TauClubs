60539666ba028291d0d8b519a643c0e7045c6437
barshopen <44550986+barshopen@users.noreply.github.com>
2021-04-19 21:14:27 +0300

    fix all current console warnings and exceptions + add label support (#31)
    * fix all current console warnings and exceptions

--- frontend/src/Scenarios/Home.js
+++ frontend/src/Scenarios/Home.js
@@ -12,7 +12,7 @@ function Home() {
   const [upcomingEvents, setUpcomingEvents] = useState();
 
   useEffect(() => {
-    fetch('http://localhost:3030/messages', {
+    fetch('http://localhost:5000/messages', {
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
@@ -21,7 +21,7 @@ function Home() {
       .then((res) => res.json())
       .then((mydata) => setMessagesData(mydata.slice(0, 7)));
 
-    fetch('http://localhost:3030/clubs', {
+    fetch('http://localhost:5000/clubs', {
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
@@ -30,7 +30,7 @@ function Home() {
       .then((res) => res.json())
       .then((mydata) => setClubsData(mydata.slice(0, 5)));
 
-    fetch('http://localhost:3030/upcoming_events', {
+    fetch('http://localhost:5000/upcoming_events', {
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
