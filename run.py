d23daeeb873cd1cb0666e3cba8071a05e6866527
barshopen <bar.shopen@gmail.com>
2021-04-25 09:34:29 +0300

    add support in pylintrc

--- run.py
+++ run.py
@@ -2,3 +2,13 @@ from server import app
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
+
+
+def bro(a):
+    a = 3
+    a = 2
+    sn = a + a
+    b = 3
+    b = b + a
+    toto = 3
+    return sn + b + toto
