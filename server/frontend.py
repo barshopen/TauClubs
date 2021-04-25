97906494a025332d67905d46dcfaed7734bd9b6c
barshopen <bar.shopen@gmail.com>
2021-04-25 20:27:32 +0300

    remove irrelvant files

--- server/frontend.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-frontend = Blueprint("frontend", __name__, static_folder="__staticbuild__")
-
-
-@frontend.route("/", methods=['GET'])
-def home():
-    return frontend.send_static_file('index.html')
