75e9bec72f5956a4e9c8f7c587b67b2f2cdef546
sharonzolty <69958637+sharonzolty@users.noreply.github.com>
2021-04-30 21:33:08 +0300

    Frontend update

--- server/auth/__init__.py
+++ server/auth/__init__.py
@@ -45,8 +45,6 @@ def index():
     if current_user.is_authenticated:
         return (
             "<p>Hello, {}! You're logged in! Email: {}</p>"
-            "<div><p>Google Profile Picture:</p>"
-            '<img src="{}" alt="Google profile pic"></img></div>'
             '<a class="button" href="/logout">Logout</a>'.format(
                 current_user.name, current_user.email, current_user.profile_pic
             )
