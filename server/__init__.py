d3e69bea9e05a2263c0629c1b6022cb5816fc010
barshopen <bar.shopen@gmail.com>
2021-04-30 13:20:10 +0300

    remove frontend blueprint - fix bug

--- server/__init__.py
+++ server/__init__.py
@@ -1,12 +1,15 @@
-from flask import Flask, abort
+import os
+from flask import Flask, abort, request
 import dotenv
 from server.db import db_app
 from server.auth import auth_app
-from server.frontend import frontend_app
-
+from server.generic import disable_route_on_flag
 dotenv.load_dotenv()
 
 app = Flask(__name__, static_folder="__staticbuild__", static_url_path="/")
+# serves static react
+FLAG_EXPECTED_VALUE = '1'
+FLAG_ACTUAL_VALUE = os.getenv('DEBUG_BACKEND')
 
 
 # blueprint for db
@@ -15,5 +18,16 @@ app.register_blueprint(db_app)
 # blueprint for auth
 app.register_blueprint(auth_app)
 
-# blueprint for frotned
-app.register_blueprint(frontend_app)
+
+@app.errorhandler(404)
+def not_found(e):
+    print(e)
+    if request.path.startswith("/api/"):
+        return "Resource not found", 404
+    return "Not found", 404
+
+
+@app.route('/')
+@disable_route_on_flag(FLAG_EXPECTED_VALUE, FLAG_ACTUAL_VALUE)
+def index():
+    return app.send_static_file('index.html')
