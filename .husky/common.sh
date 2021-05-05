ed79f1f8ba4fd2d0f00bf167c1ebd66605240432
barshopen <bar.shopen@gmail.com>
2021-05-05 15:36:18 +0300

    add a complex pre-push script

--- .husky/common.sh
+++ .husky/common.sh
@@ -1,3 +1,11 @@
+PIP=.env/Scripts/pip.exe
+REQUIREMENTS=requirements.txt
+SHOULDNT_CHANGE='package.json yarn.lock'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+
+NC='\033[0m' # No Color
+HUSKY='[HUSKY] '
 command_exists () {
   command -v "$1" >/dev/null 2>&1
 }
