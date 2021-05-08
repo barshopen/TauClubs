1c9b798ae89b10aac579f28540ad4eac2c4bdce4
sharonzolty <69958637+sharonzolty@users.noreply.github.com>
2021-05-08 15:05:25 +0300

    Update mongodb host and id field

--- server/db/users.py
+++ server/db/users.py
@@ -2,7 +2,7 @@ from mongoengine import Document, StringField, ListField, EmailField, UUIDField
 
 
 class User(Document):
-    id = UUIDField()  # consider ObjectIdField
+    # id = UUIDField()  # consider ObjectIdField
     name = StringField(max_length=200, required=True)
     contactMail = EmailField(required=True, unique=True)
     # check if can define the list
