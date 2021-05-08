d4e286519d60f2edea4de30b0098d9ac1ca3830e
Daniel Peretz <danielperetz050@gmail.com>
2021-05-08 14:01:13 +0300

    fix the review issues

--- frontend/src/Components/Feed/Feed.js
+++ frontend/src/Components/Feed/Feed.js
@@ -1,26 +1,18 @@
-import React, { useMemo, useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { useQuery } from 'react-query';
+import React, { useState, useEffect } from 'react';
 import Container from '@material-ui/core/Container';
 import FeedCard from './GenericFeedCard';
-import FeedNavBar from './FeedNavBar';
 import { getFeedData } from '../../api';
 
 const Feed = () => {
-  // const [currentTab, setCurrentTab] = useState('all');
-  // const storeKey = ['data', currentTab];
-  // const { data } = useQuery(storeKey, () => getFeedData(currentTab));
   const [data, setData] = useState();
   useEffect(() => {
     getFeedData().then(d => {
       setData(d);
-      console.log({ d });
     });
   }, []);
 
   return (
     <Container>
-      {/* <FeedNavBar setCurrentTab='all' /> */}
       <Container>
         {data?.map(
           ({ id, type, title, profileImage, description, lastUpdateTime }) => (
