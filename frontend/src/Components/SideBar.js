da552b7e5a8b98a893a72a23fa6f214facb0aa6d
Ahaiman <ahaiman194@gmail.com>
2021-05-15 10:05:17 +0300

    SideBar issues

--- frontend/src/Components/SideBar.js
+++ frontend/src/Components/SideBar.js
@@ -43,7 +43,6 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.up('sm')]: {
       width: drawerWidth,
       flexShrink: 0,
-      position: 'relative',
     },
     width: drawerWidth,
     flexShrink: 0,
@@ -51,6 +50,19 @@ const useStyles = makeStyles(theme => ({
   },
   drawerPaper: {
     width: drawerWidth,
+  },
+  footer: {
+    position: 'relative',
+    height: '40%',
+    [theme.breakpoints.up('md')]: {
+      height: '60%',
+    },
+  },
+  copyRight: {
+    marginTop: '10px',
+    wordBreak: 'break-word',
+    fontSize: '12px',
+    [theme.breakpoints.up('md')]: { fontSize: '14px' },
   },
 }));
 
@@ -73,11 +85,8 @@ SideBarListItem.defaultProps = {
   to: '/#',
 };
 
-const Copyright = () => (
-  <Typography
-    style={{ marginTop: '10px', wordBreak: 'break-word' }}
-    variant='body2'
-    color='textSecondary'>
+const Copyright = ({ className }) => (
+  <Typography className={className} variant='body2' color='textSecondary'>
     {'Copyright © '}
     <Link color='inherit' href='/'>
       TauClubs
@@ -85,6 +94,10 @@ const Copyright = () => (
     {new Date().getFullYear()}
   </Typography>
 );
+
+Copyright.propTypes = {
+  className: PropTypes.string.isRequired,
+};
 
 export default function SideBar() {
   const [clubsData, setClubsData] = useState([]);
@@ -128,20 +141,15 @@ export default function SideBar() {
           </SideBarListItem>
         ))}
       </List>
-      <Box display='flex' flexDirection='column'>
-        <Box
-          backgroundColor='background.default'
-          m={2}
-          p={2}
-          position='absolute'
-          bottom='0'>
+      <Box className={classes.footer}>
+        <Box m={2} p={2} position='absolute' bottom='0'>
           <Typography align='center' variant='body2'>
             For more information
           </Typography>
           <Box display='flex' justifyContent='center' pt={2}>
             <ContactUsModal />
           </Box>
-          <Copyright />
+          <Copyright className={classes.copyRight} />
         </Box>
       </Box>
     </div>
