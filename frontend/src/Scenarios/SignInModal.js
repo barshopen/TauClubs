3eac9decb5a16f96e5c03656c7d84d2e9ac3db53
barshopen <44550986+barshopen@users.noreply.github.com>
2021-05-15 14:07:37 +0300

    fix warnings (#85)

--- frontend/src/Scenarios/SignInModal.js
+++ frontend/src/Scenarios/SignInModal.js
@@ -1,7 +1,7 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { GoogleLogin } from 'react-google-login';
 import { useSetRecoilState } from 'recoil';
-import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
@@ -19,7 +19,7 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function SignInModal({ ClickableTrigger }) {
+function ModalContent() {
   const classes = useStyles();
   const setUser = useSetRecoilState(currentUser);
   const [loginError, setLoginError] = useState(false);
@@ -50,23 +50,23 @@ export default function SignInModal({ ClickableTrigger }) {
     setLoginError(true);
   }
 
-  function ModalContent() {
-    return (
-      <div className={classes.root}>
-        <Typography variant='h5'>Login to TauClubs</Typography>
-        <GoogleLogin
-          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-          buttonText='Log in with Google'
-          onSuccess={loginSuccess}
-          onFailure={loginFailure}
-          cookiePolicy='single_host_origin'
-          redirectUri='postmessage'
-          scope='openid'
-        />
-      </div>
-    );
-  }
+  return (
+    <div className={classes.root}>
+      <Typography variant='h5'>Login to TauClubs</Typography>
+      <GoogleLogin
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        buttonText='Log in with Google'
+        onSuccess={loginSuccess}
+        onFailure={loginFailure}
+        cookiePolicy='single_host_origin'
+        redirectUri='postmessage'
+        scope='openid'
+      />
+    </div>
+  );
+}
 
+export default function SignInModal({ ClickableTrigger }) {
   return (
     <GenericModal
       ClickableTrigger={ClickableTrigger}
