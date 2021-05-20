ca7882d28a949f18c365757ab81aca13d8b75506
Avital Haiman <57088761+Ahaiman@users.noreply.github.com>
2021-05-20 10:06:47 +0300

    Feature/fixwhoami (#89)
    * Update return dict
    
    * Update __init__.py
    
    * Recoile user full object setting
    
    * Update __init__.py
    
    * fix whoami
    
    * Update __init__.py
    
    * remove login required
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Frontend - isUser query in NavBar
    
    Co-authored-by: sharonzolty <69958637+sharonzolty@users.noreply.github.com>

--- server/auth/userauth.py
+++ server/auth/userauth.py
@@ -9,9 +9,6 @@ class UserAuth(UserMixin, Document):
     email = EmailField(required=True, unique=True)
     userauth = ReferenceField(User)
 
-    def __repr__(self):
-        return f"< User {self.name} >"
-
 
 def get_userauth_email_by_id(id: str):
     return UserAuth.objects.get(pk=ObjectId(id)).email
@@ -22,3 +19,16 @@ def create_user_auth(firstName, lastName, email, picture):
     user = UserAuth(email=email, userauth=full_user)
     user.save()
     return user
+
+
+def getUserInfo(id):
+    user_auth = UserAuth.objects.get(id=id)
+    user = user_auth.userauth
+    dict = {
+        "id": id,
+        "email": user.contactMail,
+        "firstName": user.firstName,
+        "lastName": user.lastName,
+        "picture": user.picture,
+    }
+    return dict
