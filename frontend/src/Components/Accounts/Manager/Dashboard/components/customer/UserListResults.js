a25cf23366b862fbbb7d9d3db462a7745f4897d0
barshopen <bar.shopen@gmail.com>
2021-05-26 02:21:04 +0300

    [TECH DEBT] remove (forcefully) eslint errors

--- frontend/src/Components/Accounts/Manager/Dashboard/components/customer/UserListResults.js
+++ frontend/src/Components/Accounts/Manager/Dashboard/components/customer/UserListResults.js
@@ -22,18 +22,6 @@ const UserListResults = ({ users, ...rest }) => {
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(0);
 
-  const handleSelectAll = event => {
-    let newSelectedCustomerIds;
-
-    if (event.target.checked) {
-      newSelectedCustomerIds = users.map(user => user.id);
-    } else {
-      newSelectedCustomerIds = [];
-    }
-
-    setSelectedCustomerIds(newSelectedCustomerIds);
-  };
-
   const handleSelectOne = (event, id) => {
     const selectedIndex = selectedCustomerIds.indexOf(id);
     let newSelectedCustomerIds = [];
--- frontend/src/Components/Accounts/Manager/Dashboard/components/dashboard/TotalClubs.js
+++ frontend/src/Components/Accounts/Manager/Dashboard/components/dashboard/TotalClubs.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+
 import React from 'react';
 
 import { Avatar, Card, CardContent, Grid, Typography } from '@material-ui/core';
