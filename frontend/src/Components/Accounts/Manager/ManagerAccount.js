30c729f14308d89d60c36391d8f6dcc8e5cec212
sharonzolty <69958637+sharonzolty@users.noreply.github.com>
2021-07-31 18:59:06 +0300

    Revert "Delete ManagerAccount.js"
    This reverts commit f048a750ca61405c03813bf249f15facef1e211c.

---
+++ frontend/src/Components/Accounts/Manager/ManagerAccount.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useQuery } from 'react-query';
+import BaseLoader from 'react-loader-spinner';
+import DashboardApp from './Dashboard/DashboardApp';
+import { getDashboardData } from '../../../Shared/api';
+
+const Loader = styled(BaseLoader)`
+  margin-top: 25%;
+  display: flex;
+  justify-content: center;
+`;
+
+const ManagerAccount = () => {
+  const storeKey = ['dashboardData'];
+  const { data, isLoading } = useQuery(storeKey, getDashboardData, {
+    staleTime: 0,
+  });
+
+  return isLoading ? (
+    <Loader type='TailSpin' color='#00BFFF' height={100} width={100} />
+  ) : (
+    <DashboardApp data={data} />
+  );
+};
+export default ManagerAccount;
